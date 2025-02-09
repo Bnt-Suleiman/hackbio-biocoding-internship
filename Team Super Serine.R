4c31e0c381155c831094c00b2f3b3dbfcfdc03cf
Bnt_Suleiman <suleimanzainab280@gmail.com>
2025-02-08 23:53:53 -0800

    Add files via upload

---
+++ Team Super Serine.R
@@ -0,0 +1,13 @@
+#Team Super Serine
+Team_Super_Serine <- data.frame(
+  Name = c("Samuel Badeleke", "Valerie Martins", "Zainab Suleiman"),
+  Slack_Username = c("SamuelBadeleke", "Valerie", "Bnt_Suleiman"),
+  Email = c("samuelbadekale@gmail.com", "val4martins@gmail.com", "suleimanzainb280@gmail.com"),
+  Hobby = c("Reading, Watching movies and Playing games", "Watching movies","Reading"),
+  Country = c ("Nigeria", "Nigeria", "Nigeria"),
+  Discipline = c("Cell Biology and Genetics", "Cell and Molecular Biology"),
+  Preferred_Programming_Language = c("Python", "Python", "R"),
+  
+)
+#Print Team Super Serine
+print(Team_Super_Serine)
